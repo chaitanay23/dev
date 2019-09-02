823eb7a61253f2fd58af07678c27e3964fa4fac0
Chaitanay Aggarwal <chaitanay@magicspree.com>
2019-09-02 19:14:24 +0530

    auth and user register and login

--- server.js
+++ server.js
@@ -6,7 +6,18 @@ const app = express();
 //Connect DB
 connectDb();
 
+app.use(express.json({
+    extended: false
+}));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
